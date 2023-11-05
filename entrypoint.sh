9a62d6e177d11b50c804ae94bc6dc50275589b7a
Old-Shatterhand <joeres.roman@web.de>
2023-11-05 23:03:57 +0100

    New try for platforms

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,7 @@ check_if_meta_yaml_file_exists() {
 build_package(){
 	channels=$(echo $INPUT_CHANNELS | tr "," "\n")
 	versions=$(echo $INPUT_VERSIONS | tr "," "\n")
+	platforms=$(echo $INPUT_PLATFORMS | tr "," "\n")
     
 	# may be replaced by {'python': [$INPUT_VERSIONS]}
 	build_command="conda-build --variants \"{'python': ["
@@ -43,14 +44,27 @@ build_package(){
 	
 	echo "Execute command: $build_command"
 	eval "$build_command"
-    
-	conda convert -p osx-64 linux-64/*.tar.bz2
+
+	for platform in $platforms; do
+		cp_cmd="conda convert -p $platform linux-64/*.tar.bz2"
+		echo "Convert command: $cp_cmd"
+		eval "$cp_cmd"
+	done
+	# conda convert -p osx-64 linux-64/*.tar.bz2
 }
 
 upload_package(){
-    export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    anaconda upload --label main linux-64/*.tar.bz2
-    anaconda upload --label main osx-64/*.tar.bz2
+	platforms=$(echo $INPUT_PLATFORMS | tr "," "\n")
+	export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
+	
+	anaconda upload --label main linux-64/*.tar.bz2
+	
+	for platform in $platforms; do
+		ul_cmd="anaconda upload --label main $platform/*.tar.bz2"
+		echo "Upload command: $ul_cmd"
+		eval "$cp_cmd"
+	done
+	# anaconda upload --label main osx-64/*.tar.bz2
 }
 
 go_to_build_dir
