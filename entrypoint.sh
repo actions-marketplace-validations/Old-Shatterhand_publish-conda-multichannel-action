c7d362bb9f808428e21c82e7d7865a63e89368e7
Old-Shatterhand <joeres.roman@web.de>
2023-11-05 20:54:09 +0100

    Bug fixes in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,16 +53,17 @@ build_package(){
 }
 
 upload_package(){
-	platforms=$(echo $INPUT_PLATFORMS | tr "," "\n")
+    platforms=$(echo $INPUT_PLATFORMS | tr "," "\n")
 	
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
     
-	anaconda upload --label main linux-64/*.tar.bz2
+    anaconda upload --label main linux-64/*.tar.bz2
 	
     for platform in $platforms; do
-		cmd="anaconda upload --label main $platform/*.tar.bz2"
-		echo "Upload command: $cmd"
-		eval cmd
+        cmd="anaconda upload --label main $platform/*.tar.bz2"
+        echo "Upload command: $cmd"
+        eval cmd
+    done
 }
 
 go_to_build_dir
