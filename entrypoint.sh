f5a0d836f33764efaaa4c060b0231aeecd22174d
Roman Joeres <joeres.roman@web.de>
2024-04-05 00:45:46 +0200

    Code cleaning for v0.0.11

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,6 @@
 set -ex
 set -o pipefail
 
-go_to_build_dir() {
-    if [ ! -z $INPUT_SUBDIR ]; then
-        cd $INPUT_SUBDIR
-    fi
-}
-
-check_if_meta_yaml_file_exists() {
-    if [ ! -f meta.yaml ]; then
-        echo "meta.yaml must exist in the directory that is being packaged and published."
-        exit 1
-    fi
-}
-
 build_package(){
 	channels=$(echo $INPUT_CHANNELS | tr "," "\n")
 	versions=$(echo $INPUT_VERSIONS | tr "," "\n")
@@ -49,9 +36,9 @@ build_package(){
 	# conda convert -p osx-arm64 linux-64/*.tar.bz2
 
 	for platform in $platforms; do
-	  cp_cmd="conda convert -p $platform linux-64/*.tar.bz2"
-	  echo "Convert command: $cp_cmd"
-	  eval "$cp_cmd"
+		cp_cmd="conda convert -p $platform linux-64/*.tar.bz2"
+		echo "Convert command: $cp_cmd"
+		eval "$cp_cmd"
 	done
 }
 
@@ -59,22 +46,17 @@ upload_package(){
 	platforms=$(echo $INPUT_PLATFORMS | tr "," "\n")
 	export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
 
-	if $UPLOADORIGINAL
-	then
-	  anaconda upload --label main linux-64/*.tar.bz2
-	fi
+	anaconda upload --label main linux-64/*.tar.bz2
 
 	# anaconda upload --label main osx-64/*.tar.bz2
 	# anaconda upload --label main osx-arm64/*.tar.bz2
 
 	for platform in $platforms; do
-	  ul_cmd="anaconda upload --label main $platform/*.tar.bz2"
-	  echo "Upload command: $ul_cmd"
-	  eval "$cp_cmd"
+		ul_cmd="anaconda upload --label main $platform/*.tar.bz2"
+		echo "Upload command: $ul_cmd"
+		eval "$cp_cmd"
 	done
 }
 
-go_to_build_dir
-# check_if_meta_yaml_file_exists
 build_package
 upload_package
